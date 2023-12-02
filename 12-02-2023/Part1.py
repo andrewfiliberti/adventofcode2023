88d3ce758a8d6bc4af4f1f08cbb73a8b18b2902b
andrewfiliberti <46582617+andrewfiliberti@users.noreply.github.com>
2023-12-02 12:01:56 -0500

    Add files via upload

---
+++ 12-02-2023/Part1.py
@@ -0,0 +1,29 @@
+# The Elf would first like to know which games would have been possible if the bag contained only 12 red cubes, 13 green cubes, and 14 blue cubes?
+
+import pandas as pd
+import numpy as np
+import re
+
+
+file1 = open('input.txt', 'r')
+lines = file1.readlines()
+ans = 0
+
+for line in lines:
+    maxCubes = {"red": 0, "green": 0, "blue": 0}
+    newLine = line.split(":")
+    game = int(newLine[0].split(" ")[1])
+    cubes = re.split('; |, ', newLine[1])
+    
+    for pull in cubes:
+        clean = pull.strip()
+        num = int(clean.split(" ")[0])
+        color = clean.split(" ")[1]
+        
+        if maxCubes[color] < num:
+            maxCubes[color] = num
+    
+    if maxCubes["red"] <= 12 and maxCubes["green"] <= 13 and maxCubes["blue"] <= 14:
+        ans += game
+
+print(ans)
